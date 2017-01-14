4fad5d1cb5d6a8d5eb32d7b09a1cbd201bbdf15a
ADre01 <andredelwyn@gmail.com>
2017-01-14 21:07:40 +0200

    Added angular and setup views/controllers

--- models/article.js
+++ models/article.js
@@ -36,3 +36,30 @@ module.exports.getArticleByCategory = function(category, callback){
     var query = {category : category};
     Article.find(query, callback);
 }//Get Particluar Category Articles
+
+module.exports.createArticle = function(newArticle, callback){
+    newArticle.save(callback);
+}//Add an article
+
+
+module.exports.updateArticle = function(id, data, callback){
+    var title = data.title;
+    var body = data.body;
+    var category = data.category;
+    var query = {id: id};
+    Article.findById(id, function(err, article){
+        if(!article){
+            return next(new Error("Could not load article"));
+        } else{
+            article.title = title;
+            article.body = body;
+            article.category = category;
+            article.save(callback);
+        }
+    });
+}//Update an article
+
+module.exports.removeArticle = function(id, callback){
+    Article.find({_id: id}).remove(callback);
+}
+//Remove an article
--- routes/articles.js
+++ routes/articles.js
@@ -30,6 +30,63 @@ router.get('/category/:category', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next){
+    //Get form values 
+    var title = req.body.title;
+    var category = req.body.category;
+    var body = req.body.body;
+    
+    //Article object
+    var newArticle = new Article({
+        title: title,
+        category: category,
+        body: body
+    });
+    
+    //Create Article
+    Article.createArticle(newArticle, function(err, article){
+        if(err){
+            console.log(err);
+        }
+        res.location('/articles');
+        res.redirect('/articles');
+    })
+});
+
+router.put('/', function(req, res, next){
+    var id = req.body.id;
+    var data = {
+        title: req.body.title,
+        category: req.body.category,
+        body: req.body.body
+    };
+    
+    //Create Article
+    Article.updateArticle(id, data, function(err, article){
+        if(err){
+            console.log(err);
+        }
+        res.location('/articles');
+        res.location('/articles');
+
+    });
+});
+
+router.delete('/:id', function(req, res, next){
+    var id = req.params.id;
+    Article.removeArticle(id, function(err, article){
+        if(err){
+            console.log(err);
+        }
+        res.location('/articles');
+        res.location('/articles');
+    });
+});
 
 
 module.exports = router;
+
+
+
+
+
